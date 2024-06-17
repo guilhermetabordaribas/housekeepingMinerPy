01b13131d0c9e7f312f2f9966894a2468aafe03e
guilhermetabordaribas <guilherme.ribas@gmail.com>
2024-06-17 15:20:27 -0400

    implement correlation plot

--- plot.py
+++ plot.py
@@ -1,47 +1,188 @@
 import warnings
+import itertools
 
 import pandas as pd
 import numpy as np
 import anndata as ad
-
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
+from matplotlib import colors
 import seaborn as sns
+from scipy.stats import pearsonr, false_discovery_control
+import scipy.cluster.hierarchy as sch
 
-def plot_stb_cv(adata, x:str = 'pool_cv', y:str = 'pool_stability_cv', hue:str = 'pool_mean', palette:str = None, legend:bool = False, figsize:tuple = (10,5), median_line:bool = True):
+def plot_stb_cv(adata, x:str = 'pool_cv', y:str = 'pool_stability_cv', z:str='pool_mean', hue:str = 'uclustering_cv_stb_labels', palette:str = None, legend:bool = False, figsize:tuple = (15,7), median_line:bool = True):
     fig = plt.figure(figsize=figsize)
-    gs = GridSpec(2, 2, width_ratios=[5, 1], height_ratios=[1, 5], wspace=.02, hspace=.02)
 
-    ax_clu = fig.add_subplot(gs[1,0])
-    ax_hist_cv = fig.add_subplot(gs[1,1])
-    ax_hist_mean = fig.add_subplot(gs[0,0])
+    gs = fig.add_gridspec(2,1)
+    # gs_top = GridSpec(2, 3, width_ratios=[8, 1, 8], height_ratios=[1, 5], wspace=.02, hspace=.02)
+    gs_top = gs[0].subgridspec(2, 3, width_ratios=[8, 1, 8], height_ratios=[1, 5], wspace=.02, hspace=.02)
+    ax_clu = fig.add_subplot(gs_top[1,0])
+    ax_y = fig.add_subplot(gs_top[1,1])
+    ax_x = fig.add_subplot(gs_top[0,0])
+    ax_y_bar = fig.add_subplot(gs_top[0,2])
+    ax_y_violin = fig.add_subplot(gs_top[1,2])
 
+    gs_bottom = gs[1].subgridspec(2, 3, width_ratios=[8, 1, 8], height_ratios=[1,5], wspace=.02, hspace=.02)
+    ax_x_violin = fig.add_subplot(gs_bottom[1,0])
+    ax_z_violin = fig.add_subplot(gs_bottom[1,2])
+
+    hue_order = adata.var[hue].unique()
     if hue == None:
         palette = None
-    sns.scatterplot(x=x, y=y, hue=hue, palette=palette, data=adata.var, ax=ax_clu)
+        cmap = None
+    else:
+        cmap = plt.get_cmap(palette, len(hue_order))
+        cmap = [colors.to_hex(cmap(i)) for i in range(len(hue_order))]
+        cmap = dict(zip(hue_order,cmap))
+
+    sns.scatterplot(x=x, y=y, hue=hue, palette=cmap, data=adata.var, ax=ax_clu)
     ax_clu.get_legend().set_visible(legend)
     ax_clu.spines[['right', 'top']].set_visible(False)
 
-    sns.histplot(x=x, ec='white', data=adata.var, kde=True, ax=ax_hist_mean)
-    ax_hist_mean.spines[['left','right', 'top']].set_visible(False)
-    ax_hist_mean.set_xlabel(None)
-    ax_hist_mean.set_ylabel(None)
-    ax_hist_mean.set_xticklabels([])
-    ax_hist_mean.set_yticklabels([])
-    ax_hist_mean.tick_params(left = False)
-    ax_hist_mean.grid(False)
+    sns.histplot(x=x, ec='white', data=adata.var, kde=True, ax=ax_x)
+    ax_x.spines[['left','right', 'top']].set_visible(False)
+    ax_x.set_xlabel(None)
+    ax_x.set_ylabel(None)
+    ax_x.set_xticklabels([])
+    ax_x.set_yticklabels([])
+    ax_x.tick_params(left = False)
+    ax_x.grid(False)
 
-    sns.histplot(y=y, ec='white', data=adata.var, kde=True, ax=ax_hist_cv)
-    ax_hist_cv.spines[['right', 'top','bottom']].set_visible(False)
-    ax_hist_cv.set_ylabel(None)
-    ax_hist_cv.set_xlabel(None)
-    ax_hist_cv.set_yticklabels([])
-    ax_hist_cv.set_xticklabels([])
-    ax_hist_cv.tick_params(bottom = False)
-    ax_hist_cv.grid(False)
+    sns.histplot(y=y, ec='white', data=adata.var, kde=True, ax=ax_y)
+    ax_y.spines[['right', 'top','bottom']].set_visible(False)
+    ax_y.set_ylabel(None)
+    ax_y.set_xlabel(None)
+    ax_y.set_yticklabels([])
+    ax_y.set_xticklabels([])
+    ax_y.tick_params(bottom = False)
+    ax_y.grid(False)
+
+    order = adata.var.groupby(hue).median(numeric_only=True).sort_values(y).index
+    sns.countplot(x=hue, order=order, color='gray', data=adata.var, ax=ax_y_bar)
+    ax_y_bar.set_xticklabels([])
+    ax_y_bar.set_xlabel(None)
+    ax_y_bar.spines[['left','right', 'top']].set_visible(False)
+    ax_y_bar.set_ylabel('Qty.')
+
+    sns.stripplot(x=hue, y=y, order=order, color='black', s=1, data=adata.var, ax=ax_y_violin)
+    sns.boxplot(x=hue, y=y, order=order, palette=cmap, data=adata.var, ax=ax_y_violin)
+    ax_y_violin.set_ylabel(None)
+    ax_y_violin.set_yticklabels([])
+
+    order = adata.var.groupby(hue).median(numeric_only=True).sort_values(x).index
+    sns.stripplot(x=hue, y=x, order=order, color='black', s=1, data=adata.var, ax=ax_x_violin)
+    sns.boxplot(x=hue, y=x, order=order, palette=cmap, data=adata.var, ax=ax_x_violin)
+
+    order = adata.var.groupby(hue).median(numeric_only=True).sort_values(z, ascending=False).index
+    sns.stripplot(x=hue, y=z, order=order, color='black', s=1, data=adata.var, ax=ax_z_violin)
+    sns.boxplot(x=hue, y=z, order=order, palette=cmap, data=adata.var, ax=ax_z_violin)
 
     if median_line:
         ax_clu.axvline(np.median(adata.var[x]), ls='--', lw=1,color='black')
         ax_clu.axhline(np.median(adata.var[y]), ls='--', lw=1, color='black')
 
     return None
+
+def plot_corr(adata, layer:str = None, col_labels:str = 'uclustering_cv_stb_labels', labels:list = None, r_pearson_lim:float = 0.5, p_value_lim:float = 0.05, fdr:bool = True, figsize:tuple = (7,7)):
+    if labels == None:
+        labels = []
+        labels.append(adata.var.groupby(col_labels).median(numeric_only=True).sort_values('pool_cv').index[0])
+
+    if layer != None:
+        aux = adata[:,adata.var.[col_labels].isin(labels)].layers[layer]
+    else:
+        aux = adata[:,adata.var.[col_labels].isin(labels)].X
+
+    cols_ = list(itertools.combinations_with_replacement(aux.columns,2, ))
+    P_list = []
+    pv_list = []
+    pair = []
+    for col in cols_:
+        pair.append(','.join(col))
+        P_list.append(pearsonr(aux[col[0]], aux[col[1]]).statistic)
+        pv_list.append(pearsonr(aux[col[0]], aux[col[1]]).pvalue)
+
+    if fdr:
+        pv_list = false_discovery_control(pv_list)
+    aux = pd.DataFrame([pair, P_list,pv_list], index=['gene_pair','R','pvalue']).T
+    aux[['Gene_name_x', 'Gene_name_y']] = aux.gene_pair.str.split(',', expand=True)
+    aux_2 = aux.copy()
+    aux_2.columns = ['gene_pair', 'R', 'pvalue', 'Gene_name_y', 'Gene_name_x']#
+    aux = pd.concat([aux, aux_2])
+    aux_R = aux.drop_duplicates().pivot(columns='Gene_name_x', index='Gene_name_y', values='R')
+
+    Z=sch.linkage(aux_R.astype(float), method='ward')
+    order = sch.leaves_list(Z)
+    n_gene_dict = dict(zip(aux_R.columns[order],range(len(aux_R.columns))))
+
+    aux['Gene_name_x'] = aux.Gene_name_x.map(n_gene_dict)
+    aux['Gene_name_y'] = aux.Gene_name_y.map(n_gene_dict)
+    aux['R'] = np.where((aux.R.abs().values>=r_pearson_lim), 'R Pearson ≥'+str(r_pearson_lim), 'R Pearson <'+str(r_pearson_lim))
+    aux['pvalue'] = np.where((aux.pvalue.abs().values<=p_value_lim), 'padj ≤'+str(p_value_lim), 'padj >'+str(p_value_lim))
+
+
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    sns.scatterplot(x='Gene_name_x', y='Gene_name_y', hue='R', marker='o', size='pvalue', palette=['green', 'black'], edgecolor='grey', data=aux, ax=ax)
+    ax.set_xticks(range(len(n_gene_dict.keys())), aux_R.columns[order], rotation=90, style='italic')
+    ax.set_yticks(range(len(n_gene_dict.keys())), aux_R.columns[order], style='italic')
+    ax.spines[['left', 'bottom', 'right', 'top']].set_visible(False)
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    ax.legend(bbox_to_anchor=(.85, -.175), ncol=2)
+    # ax.legend(bbox_to_anchor=(1., 1.))
+    plt.grid(True, alpha=.25)
+    plt.gca().set_axisbelow(True)
+
+    return None
+
+
+# import warnings
+#
+# import pandas as pd
+# import numpy as np
+# import anndata as ad
+#
+# import matplotlib.pyplot as plt
+# from matplotlib.gridspec import GridSpec
+# from matplotlib import cm
+# import seaborn as sns
+#
+# def plot_stb_cv(adata, x:str = 'pool_cv', y:str = 'pool_stability_cv', hue:str = 'pool_mean', palette:str = None, legend:bool = False, figsize:tuple = (10,5), median_line:bool = True):
+#     fig = plt.figure(figsize=figsize)
+#     gs = GridSpec(3, 3, width_ratios=[8, 1, 9], height_ratios=[1, 5, 5], wspace=.02, hspace=.02)
+#
+#     ax_clu = fig.add_subplot(gs[1,0])
+#     ax_hist_cv = fig.add_subplot(gs[1,1])
+#     ax_hist_mean = fig.add_subplot(gs[0,0])
+#
+#     if hue == None:
+#         palette = None
+#     sns.scatterplot(x=x, y=y, hue=hue, palette=palette, data=adata.var, ax=ax_clu)
+#     ax_clu.get_legend().set_visible(legend)
+#     ax_clu.spines[['right', 'top']].set_visible(False)
+#
+#     sns.histplot(x=x, ec='white', data=adata.var, kde=True, ax=ax_hist_mean)
+#     ax_hist_mean.spines[['left','right', 'top']].set_visible(False)
+#     ax_hist_mean.set_xlabel(None)
+#     ax_hist_mean.set_ylabel(None)
+#     ax_hist_mean.set_xticklabels([])
+#     ax_hist_mean.set_yticklabels([])
+#     ax_hist_mean.tick_params(left = False)
+#     ax_hist_mean.grid(False)
+#
+#     sns.histplot(y=y, ec='white', data=adata.var, kde=True, ax=ax_hist_cv)
+#     ax_hist_cv.spines[['right', 'top','bottom']].set_visible(False)
+#     ax_hist_cv.set_ylabel(None)
+#     ax_hist_cv.set_xlabel(None)
+#     ax_hist_cv.set_yticklabels([])
+#     ax_hist_cv.set_xticklabels([])
+#     ax_hist_cv.tick_params(bottom = False)
+#     ax_hist_cv.grid(False)
+#
+#     if median_line:
+#         ax_clu.axvline(np.median(adata.var[x]), ls='--', lw=1,color='black')
+#         ax_clu.axhline(np.median(adata.var[y]), ls='--', lw=1, color='black')
+#
+#     return None
