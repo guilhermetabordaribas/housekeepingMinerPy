36643e67d9a519a1e0f37fbc7aebfcd755cab1da
guilhermetabordaribas <guilherme.ribas@gmail.com>
2024-08-28 14:29:54 -0400

    documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx_rtd_theme']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']#,'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -27,7 +27,7 @@ autodoc_mock_imports = ['sknetwork','umap','false_discovery_control','scipy.stat
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'#'alabaster'
+html_theme = 'alabaster'#'sphinx_rtd_theme'#
 html_static_path = ['_static']
 # html_theme_options = {
 #     'logo_only': True,
