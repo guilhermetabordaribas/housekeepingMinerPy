a482d915663bde23f8a7f9c071760e638fc66281
guilhermetabordaribas <guilherme.ribas@gmail.com>
2024-08-28 14:19:18 -0400

    documentation update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
