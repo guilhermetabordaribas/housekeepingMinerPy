c13730945820d00e441ae5ab1cd9c6e220a6d37a
guilhermetabordaribas <guilherme.ribas@gmail.com>
2024-08-29 12:15:01 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,9 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-autodoc_mock_imports = ['sknetwork','umap','false_discovery_control','scipy.stats','hdbscan','pygad','boruta','statannotations','adjustText']
+autodoc_mock_imports = ['sknetwork','umap','false_discovery_control','scipy.stats','hdbscan','pygad',
+'boruta','statannotations','adjustText',
+'pandas','anndata','sklearn','scipy']#'numpy',
 
 
 # -- Options for HTML output -------------------------------------------------
